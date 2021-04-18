36960dcb75bd66bae06917e41bba732ad206547c
Oluwasanmi <sanmiaderibigbe@outlook.com>
2021-04-18 09:30:17 +0200

    Initial Commit

---
+++ Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by sanmi_personal on 18/04/2021.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
